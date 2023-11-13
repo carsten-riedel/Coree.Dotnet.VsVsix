34f4006232cf6d199048ede919e1f6420e9b3a3a
Carsten Riedel <carsten.riedel.one@outlook.com>
2023-11-13 23:06:58 +0100

    update

--- Program.cs
+++ Program.cs
@@ -12,7 +12,10 @@ namespace Coree.Dotnet.VsVsix
                 var EnviromentWithoutCaller = Environment.CommandLine.TrimStart(Assembly.GetExecutingAssembly().Location.ToCharArray()).TrimStart();
                 Console.WriteLine($@"This is just a wrapper command please see. https://github.com/microsoft/vsixbootstrapper");
                 Console.WriteLine($@"VSIXBootstrapper.exe {EnviromentWithoutCaller}");
-                System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo() { FileName = "VSIXBootstrapper.exe", Arguments = EnviromentWithoutCaller, WorkingDirectory=@$"{AppDomain.CurrentDomain.BaseDirectory}..\..\..\",UseShellExecute=true });
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo() { FileName = "VSIXBootstrapper.exe", Arguments = EnviromentWithoutCaller, WorkingDirectory = @$"{AppDomain.CurrentDomain.BaseDirectory}..\..\..\", UseShellExecute = true };
+                var proc = new System.Diagnostics.Process(){ StartInfo = startInfo };
+                proc.Start();
+                proc.WaitForExit();
             }
             else
             {
